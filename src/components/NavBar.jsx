ee11747cf27ac96caaad79ae9d395ba933b3eddf
Chango961204 <metallicaluis73@gmail.com>
2024-09-11 15:16:09 -0600

    NavBar transparent

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
-import { Navbar, Nav, NavDropdown, Button, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import Logo from '../assets/LogoA.png';
 import './Navbar.css';
 
 const CustomNavbar = () => {
   const [expanded, setExpanded] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY === 0 && scrolling === true) {
+      setScrolling(false);
+    } else if (window.scrollY !== 0 && scrolling !== true) {
+      setScrolling(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [scrolling]);
 
   return (
     <Navbar
       expanded={expanded}
       expand="lg"
-      className="Navbar"
+      className={`Navbar ${scrolling ? 'dark' : 'transparent'}`}
       onToggle={(isExpanded) => setExpanded(isExpanded)}
+      fixed="top"
     >
       <Container>
         <Navbar.Brand href="/" className="Navbar-brand">
@@ -20,13 +35,13 @@ const CustomNavbar = () => {
 
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
-          onClick={() => setExpanded(expanded ? false : "expanded")}
+          onClick={() => setExpanded(expanded ? false : 'expanded')}
         />
-        
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto Navbar-menu">
             <Nav.Link href="/">Inicio</Nav.Link>
-            <Nav.Link href="/about">¿Quienes Somos?</Nav.Link>
+            <Nav.Link href="/about">¿Quiénes Somos?</Nav.Link>
             <Nav.Link href="/contact">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
