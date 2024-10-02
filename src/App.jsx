d247c62b23e877e89893d56f701b6b08403a5a9c
Chango961204 <metallicaluis73@gmail.com>
2024-10-02 13:33:21 -0600

    azonix fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/NavBar.jsx';
 import AppRoutes from './routes/routes';
-import Footer from './components/Footer'; // Importa el componente Footer
+import Footer from './components/Footer'; 
 import './App.css';
 import './styles/styles.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
