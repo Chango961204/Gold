5be468570d583788fa64b90fa90da10f5587d8ca
Chango961204 <metallicaluis73@gmail.com>
2024-10-03 11:47:52 -0600

    importacion rutas en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/NavBar.jsx';
-import AppRoutes from './routes/routes';
-import Footer from './components/Footer'; 
+import AppRoutes from './routes/routes.jsx';
+import Footer from './components/Footer.jsx'; 
 import './App.css';
 import './styles/styles.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Asegura que las rutas se manejen correctamente en producción
+  base: '/', 
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
