2ca3f412eb1edc9a2dc0feda2ddddb08630189bc
Chango961204 <metallicaluis73@gmail.com>
2024-10-07 14:11:29 -0600

    run build

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Cambia el import a 'react-dom/client'
-import App from './App';
+import ReactDOM from 'react-dom/client'; 
+import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
